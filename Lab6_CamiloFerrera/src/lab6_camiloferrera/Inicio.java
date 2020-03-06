dba3d0d7ab082aba78eb904f738a3a6f02040811
camiloeferrera <camiloferrera@hotmail.com>
2020-03-06 14:35:01 -0600

    Se puede iniciar sesion y enviar mensajes pero sin respuesta de la maquina todavia

--- Lab6_CamiloFerrera/src/lab6_camiloferrera/Inicio.java
+++ Lab6_CamiloFerrera/src/lab6_camiloferrera/Inicio.java
@@ -2,9 +2,11 @@ package lab6_camiloferrera;
 
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
@@ -14,6 +16,7 @@ public final class Inicio extends javax.swing.JFrame {
     public Inicio() throws IOException {
         initComponents();
         this.setLocationRelativeTo(null);
+        usuarios.add(new Usuario ("Camilo Ferrera","cferrera","java123",19));
         EscribirArchivoTexto();
     }
 
@@ -32,6 +35,14 @@ public final class Inicio extends javax.swing.JFrame {
         sp_edadregistro = new javax.swing.JSpinner();
         jLabel8 = new javax.swing.JLabel();
         jButton3 = new javax.swing.JButton();
+        Mensajeria = new javax.swing.JDialog();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        ta_mensaje = new javax.swing.JTextArea();
+        jButton4 = new javax.swing.JButton();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        ta_chat = new javax.swing.JTextArea();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        ta_contactos = new javax.swing.JTextArea();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
@@ -122,7 +133,76 @@ public final class Inicio extends javax.swing.JFrame {
                 .addContainerGap(49, Short.MAX_VALUE))
         );
 
+        Mensajeria.setTitle("Mensajeria");
+        Mensajeria.setResizable(false);
+        Mensajeria.addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowClosing(java.awt.event.WindowEvent evt) {
+                MensajeriaWindowClosing(evt);
+            }
+        });
+
+        ta_mensaje.setColumns(20);
+        ta_mensaje.setRows(5);
+        jScrollPane1.setViewportView(ta_mensaje);
+
+        jButton4.setFont(new java.awt.Font("Dialog", 1, 14)); // NOI18N
+        jButton4.setText("Enviar");
+        jButton4.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButton4MouseClicked(evt);
+            }
+        });
+
+        ta_chat.setEditable(false);
+        ta_chat.setColumns(20);
+        ta_chat.setRows(5);
+        ta_chat.setBorder(javax.swing.BorderFactory.createTitledBorder("Conversación"));
+        jScrollPane2.setViewportView(ta_chat);
+
+        ta_contactos.setEditable(false);
+        ta_contactos.setColumns(20);
+        ta_contactos.setLineWrap(true);
+        ta_contactos.setRows(5);
+        ta_contactos.setBorder(javax.swing.BorderFactory.createTitledBorder("Contactos"));
+        jScrollPane3.setViewportView(ta_contactos);
+
+        javax.swing.GroupLayout MensajeriaLayout = new javax.swing.GroupLayout(Mensajeria.getContentPane());
+        Mensajeria.getContentPane().setLayout(MensajeriaLayout);
+        MensajeriaLayout.setHorizontalGroup(
+            MensajeriaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(MensajeriaLayout.createSequentialGroup()
+                .addGap(21, 21, 21)
+                .addGroup(MensajeriaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 512, Short.MAX_VALUE)
+                    .addComponent(jScrollPane1))
+                .addGroup(MensajeriaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(MensajeriaLayout.createSequentialGroup()
+                        .addGap(18, 18, 18)
+                        .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 141, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(MensajeriaLayout.createSequentialGroup()
+                        .addGap(33, 33, 33)
+                        .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 95, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(23, Short.MAX_VALUE))
+        );
+        MensajeriaLayout.setVerticalGroup(
+            MensajeriaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, MensajeriaLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(MensajeriaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane3)
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 351, Short.MAX_VALUE))
+                .addGap(18, 18, 18)
+                .addGroup(MensajeriaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(MensajeriaLayout.createSequentialGroup()
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 105, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, MensajeriaLayout.createSequentialGroup()
+                        .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(40, 40, 40))))
+        );
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Log In");
 
         jLabel1.setFont(new java.awt.Font("Dialog", 1, 24)); // NOI18N
         jLabel1.setText("Lab#6 Camilo Ferrera");
@@ -135,6 +215,11 @@ public final class Inicio extends javax.swing.JFrame {
 
         jButton1.setFont(new java.awt.Font("Dialog", 1, 14)); // NOI18N
         jButton1.setText("Iniciar Sesión");
+        jButton1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButton1MouseClicked(evt);
+            }
+        });
 
         jButton2.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
         jButton2.setText("Registrarse");
@@ -202,15 +287,16 @@ public final class Inicio extends javax.swing.JFrame {
             String user = tf_userregistro.getText();
             String pass = pf_passregistro.getText();
             int edad = (int)sp_edadregistro.getValue();
-            boolean valido = true;
+            boolean flag = true;
             for (int i = 0; i < usuarios.size(); i++) {
                 if (user.equals(usuarios.get(i).getUser())) {
-                    valido = false;
+                    flag = false;
+                    login = i;
                     break;
                 }
             }
             
-            if (valido == true) {
+            if (flag == true) {
                 usuarios.add(new Usuario(nombre,user,pass,edad));
                 JOptionPane.showMessageDialog(Registro, "Registrado Exitosamente!");
                 Registro.setVisible(false);
@@ -233,6 +319,53 @@ public final class Inicio extends javax.swing.JFrame {
         Registro.setLocationRelativeTo(this);
         Registro.setVisible(true);
     }//GEN-LAST:event_jButton2MouseClicked
+
+    private void jButton1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton1MouseClicked
+        if (tf_user.getText().isEmpty() || pf_pass.getText().isEmpty()) {
+            JOptionPane.showMessageDialog(this,"Hay campos vacios.");
+        } else {
+            String user = tf_user.getText();
+            String pass = pf_pass.getText();
+            boolean flag = false;
+            for (Usuario x : usuarios) {
+                if (user.equals(x.getUser()) && pass.equals(x.getPassword())) {
+                    flag = true;
+                    break;
+                }
+            }
+            
+            if (flag == true) {
+                this.setVisible(false);
+                LimpiarRegistro();
+                try {
+                    LeerContactos();
+                } catch (FileNotFoundException ex) {
+                    Logger.getLogger(Inicio.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                Mensajeria.pack();
+                Mensajeria.setLocationRelativeTo(this);
+                Mensajeria.setVisible(true);
+            } else {
+                JOptionPane.showMessageDialog(this, "Usuario y/o Contraseña Incorrecto/s.");
+            }
+        }
+    }//GEN-LAST:event_jButton1MouseClicked
+
+    private void MensajeriaWindowClosing(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_MensajeriaWindowClosing
+        System.exit(0);
+    }//GEN-LAST:event_MensajeriaWindowClosing
+
+    private void jButton4MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton4MouseClicked
+        if (ta_mensaje.getText().isEmpty()) {
+            JOptionPane.showMessageDialog(Mensajeria, "El mensaje no puede ir vacio.");
+        } else {
+            String flag = ta_chat.getText();
+            String mensaje = usuarios.get(login).getUser() + ": " + ta_mensaje.getText() +"\n";
+            String chat = flag + mensaje ;
+            ta_chat.setText(chat);
+            ta_mensaje.setText("");
+        }
+    }//GEN-LAST:event_jButton4MouseClicked
 
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
@@ -271,10 +404,12 @@ public final class Inicio extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JDialog Mensajeria;
     private javax.swing.JDialog Registro;
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
@@ -283,14 +418,21 @@ public final class Inicio extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
     private javax.swing.JLabel jLabel8;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane3;
     private javax.swing.JPasswordField pf_pass;
     private javax.swing.JPasswordField pf_passregistro;
     private javax.swing.JSpinner sp_edadregistro;
+    private javax.swing.JTextArea ta_chat;
+    private javax.swing.JTextArea ta_contactos;
+    private javax.swing.JTextArea ta_mensaje;
     private javax.swing.JTextField tf_nombreregistro;
     private javax.swing.JTextField tf_user;
     private javax.swing.JTextField tf_userregistro;
     // End of variables declaration//GEN-END:variables
     ArrayList <Usuario> usuarios = new ArrayList ();
+    int login;
     
     public void LimpiarRegistro(){
         tf_nombreregistro.setText("");
@@ -314,5 +456,17 @@ public final class Inicio extends javax.swing.JFrame {
             fw.close();
             bw.close();
         }       
+    }
+    
+    public void LeerContactos() throws FileNotFoundException{
+        Scanner sc = new Scanner (new File ("./Users.txt"));
+        String contactos = "";
+        while (sc.hasNextLine()) {
+            String x = sc.nextLine();
+            String[] y = x.split(";");
+            contactos += y[0] + "\n";
+        }
+        
+        ta_contactos.setText(contactos);
     }
 }
